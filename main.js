3b40f60106e67dbadbe5df1644474b750dc474e0
Evrim Ciftci <122287326+EvrimCiftci@users.noreply.github.com>
2024-10-24 19:24:34 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,71 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
-// 1. create the scene
+const canvas = document.getElementById('canvas');
+
+// 1.Scene
 const scene = new THREE.Scene();
-
 scene.background = new THREE.Color('#F0F0F0');
 
+// 2.Camera
 
-// 2. add the camera
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1,1000);
+const camera = new THREE.PerspectiveCamera(75,  window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-// 3.create and add a cube object
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshLambertMaterial({color: '#468585', emissive:'#468585'});
+// 3.Object
+ 
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({ color: '#468585',emissive:'#468585'});
+const dodecahedron = new THREE.Mesh(geometry, material);
 
-const cube = new THREE.Mesh(geometry, material);
+// second object
+// emissive parameter can not be used with basic material
+const BoxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const BoxMaterial = new THREE.MeshBasicMaterial({ color: '#B4B4B3' });
+const box = new THREE.Mesh(BoxGeometry, BoxMaterial);
+box.position.y = -1.5;
 
-scene.add(cube);
+scene.add(dodecahedron);
+scene.add(box);
 
-// 4. add lighting
+// 4.Light
+const ambientLight = new THREE.AmbientLight(0x006769, 100);
+ambientLight.position.set(1, 1, 1);
+scene.add(ambientLight);
 
-const light = new THREE.DirectionalLight(0x9CDBA6, 10);
-light.position.set(1, 1, 1);
-scene.add(light);
-
-// 5.set up renderer
-
-const renderer = new THREE.WebGLRenderer();
+//5.Renderer
+const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-// 6.animate the scene
+//6. Add OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
 
+//7.Animation
 function animate() {
-  
+
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.01;
+
+  box.rotation.y += 0.01;
+ 
+  controls.update();
+ 
   renderer.render(scene, camera);
+  
 }
 
-animate();  
+// 8. Handle window resizing
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  render.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
